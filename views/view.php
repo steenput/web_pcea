8de80f7832f6d6d41820d7ba002fb17dcda0f279
steven.liatti <steven.liatti@etu.hesge.ch>
2017-09-24 12:03:15 +0200

    Basic folder structure for Silex
    On branch master
    
    Changes to be committed:
    	new file:   .gitignore
    	new file:   app/routes.php
    	new file:   composer.json
    	new file:   composer.lock
    	renamed:    sql/content.sql -> db/content.sql
    	renamed:    sql/schema.mwb -> db/schema.mwb
    	renamed:    sql/schema.sql -> db/schema.sql
    	new file:   src/model.php
    	new file:   views/view.php
    	new file:   web/.htaccess
    	new file:   web/index.php

---
+++ views/view.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>Petits comptes entre amis</title>
+</head>
+<body>
+	<header>
+		<h1>Petits comptes entre amis</h1>
+	</header>
+
+	<footer class="footer">
+		
+	</footer>
+</body>
+</html>
